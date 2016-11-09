788c04e923409459888ced9e26e7573cd320b7c5
Aidan Valente <valentea19@mail.wlu.edu>
2016-11-09 13:15:04 -0500

    ruby test 11/9

--- valente/ruby/ex11.rb
+++ valente/ruby/ex11.rb
@@ -1,8 +1,8 @@
-print "How old are you? "
+puts "How old are you? "
 age = gets.chomp
-print "How tall are you? "
+puts "How tall are you? "
 height = gets.chomp
-print "How much do you weigh? "
+puts "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
