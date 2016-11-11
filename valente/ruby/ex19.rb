0f9f3b1621b494a8175eae63c71badfc371faa02
Aidan Valente <valentea19@mail.wlu.edu>
2016-11-11 13:31:08 -0500

    11/10 ruby stuff

---
+++ valente/ruby/ex19.rb
@@ -0,0 +1,17 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers)
+  puts "You have #{cheese_count} cheeses!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
+  puts "Man that's enough for a party!"
+  puts "Get a blanket.\n"
+end
+
+
+puts "We can just give the function numbers directly:"
+cheese_and_crackers(20, 30)
+
+
+puts "We can even do math inside too:"
+cheese_and_crackers(10 + 20, 5 +6)
+
+
+cheese_and_crackers()
