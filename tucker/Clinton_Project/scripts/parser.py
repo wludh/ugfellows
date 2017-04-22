0f5e5128f09e568d95476cf03780ebc28ca3cfb2
Will <tuckerw19@mail.wlu.edu>
2017-04-22 14:44:44 -0400

    parser.py defined by argv

---
+++ tucker/Clinton_Project/scripts/parser.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+from sys import argv
+script, html_file, filename = argv
+#will parse email partially. certain tags will remain above and below email
+soup = BeautifulSoup(open(html_file))
+
+
+refined = soup.find_all("div", class_="tab-content")
+refined_string = str(refined)
+for text in refined:
+    print (refined)
+target = open(filename,'w')
+target.write(refined_string)
+target.close()
