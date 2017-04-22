dee9cb26df9de4335beda08ec0df33834fa3bde8
Will <tuckerw19@mail.wlu.edu>
2017-04-22 15:27:36 -0400

    deleted 14333_content.py--replaced by parser.py

--- tucker/Clinton_Project/scripts/parser.py
+++ tucker/Clinton_Project/scripts/parser.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from sys import argv
 script, html_file, filename = argv
 #will parse email partially. certain tags will remain above and below email
+#need html to cycle through each html doc in folder
+#need filename to be defined 
 soup = BeautifulSoup(open(html_file))
 
 
@@ -9,6 +11,6 @@ refined = soup.find_all("div", class_="tab-content")
 refined_string = str(refined)
 for text in refined:
     print (refined)
-target = open(filename,'w')
+target = open(filename,'w+')
 target.write(refined_string)
 target.close()
